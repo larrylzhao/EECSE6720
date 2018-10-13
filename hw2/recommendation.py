32352d1405d690a4b2ff33e1dad0f2ea726bae97
larryzhao <larrylzhao@gmail.com>
2018-10-12 22:10:55 -0500

    hw2: checkpoint for updating U and V

--- hw2/recommendation.py
+++ hw2/recommendation.py
@@ -17,9 +17,15 @@ N, M, r_max = input.max(axis=0)
 
 
 # convert input data to a dict
-input_dict = {}
+input_dict_u = {}
+input_dict_v = {}
 for line in input:
-    input_dict[(line[0],line[1])] = line[2]
+    if line[0] not in input_dict_u.keys():
+        input_dict_u[line[0]] = {}
+    input_dict_u[line[0]][line[1]] = line[2]
+    if line[1] not in input_dict_v.keys():
+        input_dict_v[line[1]] = {}
+    input_dict_v[line[1]][line[0]] = line[2]
 
 
 # initialize U and V from Normal(0, 0.1I)
@@ -29,14 +35,15 @@ U_old = {}
 V_old = {}
 
 for i in range(1, N+1):
-    U[i] = np.random.normal(0, 0.1, d)
+    U[i] = np.matrix(np.random.normal(0, 0.1, d)).T
 for j in range(1, M+1):
-    V[i] = np.random.normal(0, 0.1, d)
-print(U[1][0], U[1][1], U[1][2], U[1][3], U[1][4])
+    V[i] = np.matrix(np.random.normal(0, 0.1, d)).T
 
+
+Ephi = {}
 
 def EPhi(i, j):
-    r_ij = input_dict(i, j)
+    r_ij = input_dict_u[i][j]
     dot_product = np.dot(np.transpose(U_old[i]), V_old[j])
     pdf = norm.pdf(-1 * dot_product)
     cdf = norm.cdf(-1 * dot_product)
@@ -44,3 +51,26 @@ def EPhi(i, j):
         return pdf/(1-cdf) + dot_product
     else:
         return pdf/cdf + dot_product
+
+identity = np.identity(d, dtype=float)
+
+def update_U():
+    for i in range(1, N+1):
+        term_1 = identity
+        term_2 = np.zeros((d, 1), dtype=float)
+        for j in input_dict_u[i].keys():
+            term_1 = term_1 + V[j].dot(V[j].T)
+            term_2 = term_2 + V[j]*Ephi[(i, j)]
+        U[i] = np.linalg.inv(term_1).dot(term_2)
+
+def update_V():
+    for j in range(1, M+1):
+        term_1 = identity
+        term_2 = np.zeros((d, 1), dtype=float)
+        for i in input_dict_v[j].keys():
+            term_1 = term_1 + U[i].dot(U[i].T)
+            term_2 = term_2 + U[i]*Ephi[(i, j)]
+        V[j] = np.linalg.inv(term_1).dot(term_2)
+
+
+update_U()
--- scratch.py
+++ scratch.py
@@ -1,3 +1,26 @@
-import math
+import numpy as np
 
-print(math.factorial(2000))
+d = 5
+
+u = np.matrix(np.random.normal(0, 0.1, d)).T
+
+identity = np.identity(d, dtype=float)
+term_1 = np.linalg.inv(identity + u.dot(u.T))
+term_2 = u*2
+total = term_1.dot(term_2)
+
+# print(term_1)
+# print()
+print(total)
+
+print(np.linalg.inv(term_1))
+
+temp = np.zeros((d, d), dtype=float)
+
+print(temp)
+
+print(u)
+
+print(u.T)
+
+print(u)
